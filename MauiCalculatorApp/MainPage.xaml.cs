0b9c7d06833c657ce0465cfb37b4b6f9b9a39f12
Emilio Barrera Sepúlveda <barrera_emilio@yahoo.es>
2023-07-29 10:37:03 -0500

    Add project files.

---
+++ MauiCalculatorApp/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiCalculatorApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
